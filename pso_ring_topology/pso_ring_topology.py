77b89057dbc0873a8cfde6067842d209118747ca
Pawel Ciupka <pawelciupka@gmail.com>
2021-08-08 20:02:08 +0200

    Naprawiono topologię pierścienia

--- pso_ring_topology/pso_ring_topology.py
+++ pso_ring_topology/pso_ring_topology.py
@@ -12,19 +12,22 @@ class PsoRingTplgy(PsoParent):
     def update_velocity_and_position(self, iter):
         # Zaktualizuj prędkość i pozycję wszystkich cząsteczek w roju
         #
-        for i in range(0, self.num_particles):
+        for i in range(0, self.num_particles): 
             neighborhoods_pos_best = self.swarm[i].position
             neighborhoods_value_best = self.swarm[i].value
 
+
             # Znajdź najlepsze położenie wśród sąsiadów
-            for j in range(0, int(self.num_neighborhoods/2)):
+            for j in range(1, int(self.num_neighborhoods/2 + 1)):
                 index1 = (i+j) % self.num_particles
                 if self.swarm[index1].value < neighborhoods_value_best:
                     neighborhoods_pos_best = list(self.swarm[index1].position)
+                    neighborhoods_value_best = self.swarm[index1].value
 
                 index2 = (i-j) % self.num_particles
                 if self.swarm[index2].value < neighborhoods_value_best:
                     neighborhoods_pos_best = list(self.swarm[index2].position)
+                    neighborhoods_value_best = self.swarm[index2].value
 
             self.swarm[i].update_velocity(neighborhoods_pos_best, iter)
             self.swarm[i].update_position(self.bounds)
